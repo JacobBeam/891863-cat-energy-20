41a5dabd5aa6f350d437d12a1bec426c1e3b7195
Pavlov Vitalii <pavlov-vv86@yandex.ru>
2020-08-22 01:43:39 +0300

    Добавлеие интерактивной карты

--- source/js/script.js
+++ source/js/script.js
@@ -8,3 +8,30 @@ burgerButton.addEventListener("click", function () {
   mainMenu.classList.toggle("menu--opened");
   burgerButton.classList.toggle("header__burger--inactive")
 })
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var mapEl = document.querySelector("#map");
+
+  if (mapEl) {
+    ymaps.ready(init); // активируем карту после загрузки страницы
+    var map;
+
+    function init() {
+      map = new ymaps.Map("map", { // в кавычках id элемента куда загружается карта
+        center: [59.9386332382438, 30.32308101654053],
+        // координаты центра фрагмента карты, подобрать можно на https://vk.cc/9n163G
+        zoom: 16,
+        controls: []
+      });
+      var placemark = new ymaps.Placemark([59.9386332382438, 30.32308101654053], { // координаты метки
+        hintContent: "ул. Большая Конюшенная, д. 19/8",
+        iconLayout: "default#image",
+        iconImageHref: "../img/map-pin.png", // иконка метки
+        iconImageSize: [56, 52], // размер метки
+
+      });
+      map.geoObjects.add(placemark);
+    }
+  }
+});
